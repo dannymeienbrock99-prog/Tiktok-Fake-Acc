404314127f4b9754e31d6e289814ee89d78e321b
Crazy_Batto <dannymeienbrock99@gmail.com>
2026-09-03 07:08:08 +0200

    Add secure preload bridge

---
+++ src/preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('appAPI', {
+  getSetting: (key) => ipcRenderer.invoke('settings:get', key),
+  setSetting: (key, value) => ipcRenderer.invoke('settings:set', key, value),
+  listProfiles: () => ipcRenderer.invoke('profiles:list'),
+  saveProfile: (profile) => ipcRenderer.invoke('profiles:save', profile)
+});
