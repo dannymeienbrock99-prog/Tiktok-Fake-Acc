e24cadca3d00c5d4ffe1cf4e1deac00a407d5916
Crazy_Batto <dannymeienbrock99@gmail.com>
2026-09-03 07:09:07 +0200

    Expose profile fetching to renderer

--- src/preload.js
+++ src/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('appAPI', {
   getSetting: (key) => ipcRenderer.invoke('settings:get', key),
   setSetting: (key, value) => ipcRenderer.invoke('settings:set', key, value),
   listProfiles: () => ipcRenderer.invoke('profiles:list'),
-  saveProfile: (profile) => ipcRenderer.invoke('profiles:save', profile)
+  saveProfile: (profile) => ipcRenderer.invoke('profiles:save', profile),
+  fetchProfile: (provider, handle) => ipcRenderer.invoke('profile:fetch', provider, handle)
 });
